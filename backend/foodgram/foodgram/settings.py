f34a1a13ca1d9149e1e1fefb5bef7f6f7ff8760e
Lisus Lev <lisus.lev@yandex.ru>
2023-01-26 14:01:37 +0200

    wf5

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE',
-                              default='django.db.backends.postgresql'),
+                            default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME',
                           default='postgres'),
         'USER': os.getenv('POSTGRES_USER',
