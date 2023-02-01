c9a40616579eefc2ec71185fdc9046d8e4a85839
Lisus Lev <lisus.lev@yandex.ru>
2023-02-02 01:22:29 +0200

    19

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -139,10 +139,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return response
 
 
-# class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
-#     queryset = Ingredient.objects.all()
-#     serializer_class = IngredientSerializer
-#     search_fields = ('^name',)
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    search_fields = ('^name',)
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
@@ -152,19 +152,19 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
         return Tag.objects.all()
 
 
-class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = IngredientSerializer
+# class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+#     serializer_class = IngredientSerializer
 
-    def get_queryset(self):
-        queryset = Ingredient.objects.all()
-        name = self.request.query_params.get('name')
-        if name is not None:
-            qs_starts = queryset.filter(name__istartswith=name)
-            qs_contains = queryset.filter(
-                ~Q(name__istartswith=name) & Q(name__icontains=name)
-            )
-            queryset = list(qs_starts) + list(qs_contains)
-        return queryset
+#     def get_queryset(self):
+#         queryset = Ingredient.objects.all()
+#         name = self.request.query_params.get('name')
+#         if name is not None:
+#             qs_starts = queryset.filter(name__istartswith=name)
+#             qs_contains = queryset.filter(
+#                 ~Q(name__istartswith=name) & Q(name__icontains=name)
+#             )
+#             queryset = list(qs_starts) + list(qs_contains)
+#         return queryset
 
 
 class UserRecipeViewSet(
