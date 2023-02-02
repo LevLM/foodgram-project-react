a6d1efe6e50a757b1d471f74d568bc9d3859ea43
Lisus Lev <lisus.lev@yandex.ru>
2023-02-02 17:56:47 +0200

    21

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -94,7 +94,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         author_id = self.request.query_params.get("author")
         if author_id:
             queryset = queryset.filter(author__id=author_id).all()
-        return queryset
 
     @action(detail=True,
             methods=['post', 'delete'],
