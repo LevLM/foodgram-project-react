3319ddb5d9ce7ebc1ec5463db0a9dffafb6eebb9
Lisus Lev <lisus.lev@yandex.ru>
2023-02-02 12:55:44 +0200

    20

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -5,7 +5,6 @@ from rest_framework import routers
 from .views import (IngredientViewSet, RecipeViewSet, TagViewSet,
                     TokenCreateView, UserRecipeViewSet)
 
-
 router = routers.DefaultRouter()
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 router.register(r'ingredients', IngredientViewSet, basename='ingredients')
