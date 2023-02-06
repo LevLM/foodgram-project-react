a5d13901404aacec55d25b36e6232dfdcd8236ac
Lisus Lev <lisus.lev@yandex.ru>
2023-02-06 12:02:06 +0200

    fix buggs 1

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@p7e4(u%)pz9ukqf18_0__hwrjn#59dwo&av)7^#nh=18t0t_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '158.160.43.47', 'll-praktik.ddns.net', '*'
@@ -71,27 +71,27 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE',
-                            default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME',
-                          default='foodgram'),
-        'USER': os.getenv('POSTGRES_USER',
-                          default='admin'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD',
-                              default='ghbdtn11'),
-        'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default='5432')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE',
+#                             default='django.db.backends.postgresql'),
+#         'NAME': os.getenv('DB_NAME',
+#                           default='foodgram'),
+#         'USER': os.getenv('POSTGRES_USER',
+#                           default='admin'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD',
+#                               default='ghbdtn11'),
+#         'HOST': os.getenv('DB_HOST', default='db'),
+#         'PORT': os.getenv('DB_PORT', default='5432')
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
